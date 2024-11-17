8e4035c03494a4939376cc907033d83410079b87
Nick Sullivan <nsulmol@gmail.com>
2024-11-17 14:02:16 -0500

    [feat] modify n_to_m to read imgs from JSON
    - We modified the functions so that we can load the images from the json
    file, ensuring there is a 1-to-1 mapping.
    - We modified the functions so they return list[np.array] rather than a
    3D array that is a stack. We only stack *before* calling the synthesis function.

--- n_to_m.py
+++ n_to_m.py
@@ -16,6 +16,7 @@ INDIR = './data/'  # './data/'
 OUTDIR = './syn/'  # './syn/'
 EXT = '.png'
 LABELS_FNAME = 'labels.json'
+IMAGE_PATH = '/home/nsulmol/.local/share/label-studio/media/upload/1'
 
 
 def normalize_image(arr: np.array) -> np.array:
@@ -35,32 +36,41 @@ def convert_to_uint16(arr: np.array) -> np.array:
     return arr.astype(np.uint16)
 
 
-def load_images(path: str, ext: str = None) -> np.array:
-    """ Loads all images in a directory, normalizes, and stacks."""
-    # Get files
-    files = [f for f in os.listdir(path) if
+def get_images_filepaths(path: str, ext: str = None) -> list[str]:
+    """Given a directory and extension, extract list of image filepaths."""
+    files = [os.path.join(path, f) for f in os.listdir(path) if
              os.path.isfile(os.path.join(path, f))]
-
     if ext:
         files = [f for f in files if ext in f]
+    return files
 
+
+def load_images_from_dir(path: str, ext: str = None) -> list[np.array]:
+    """ Loads all images in a directory, normalizes, and returns."""
+    file_list = get_images_filepaths(path, str)
+    return load_images_from_file_list(file_list)
+
+    # Convert to stacked array
+    #return np.stack(tuple(arrs))
+
+
+def load_images_from_file_list(filepaths: list[str]) -> list[np.array]:
     # Load and convert to numpy
     arrs = []
-    for f in files:
-        img_src = Image.open(os.path.join(path, f))
+    for f in filepaths:
+        img_src = Image.open(f)
         img_src = np.array(img_src)
 
         # Whiten before normalizing...
         img_src = scattering.whiten(img_src[np.newaxis, ...])[0, ...]
         img_src = normalize_image(img_src)
         arrs.append(img_src)
-
-    # Convert to stacked array
-    return np.stack(tuple(arrs))
+    return arrs
 
 
-def load_labels(fname: str) -> list[list[int, int, int, int]]:
-    """Extract labels from JSON labels file."""
+def load_labels_images(fname: str, img_path: str
+                       ) -> (list[list[int, int, int, int]], list[np.array]):
+    """Extract labels and images from JSON labels file (with path to images)."""
     # Hardcoded crud (for now)
     ANNOTATIONS_STR = 'annotations'
     RESULT_STR = 'result'
@@ -78,8 +88,10 @@ def load_labels(fname: str) -> list[list[int, int, int, int]]:
         labelset = json.load(f)
 
     total_labels = []  # Will hold x1, x2, y1, y2
+    filepaths = []
     for img_dict in labelset:
         print(f'Filename: {img_dict[FILENAME_STR]}')
+        filepaths.append(os.path.join(img_path, img_dict[FILENAME_STR]))
         img_labels = []
 
         annotations = img_dict[ANNOTATIONS_STR][0][RESULT_STR]
@@ -97,34 +109,34 @@ def load_labels(fname: str) -> list[list[int, int, int, int]]:
                             h * values[HEIGHT_STR] / 100)]
             img_labels.append(x1x2y1y2)
         total_labels.append(img_labels)
-    return total_labels
+
+    arrs = load_images_from_file_list(filepaths)
+    return total_labels, arrs
 
 
-def mask_images(arrs: np.array, total_labels: list[list[int, int, int, int]]
-                ) -> np.array:
+def mask_images(arrs: list[np.array], total_labels: list[list[int, int, int, int]]
+                ) -> list[np.array]:
     """Mask out labels from all provided arrays.
 
     Assumes the shape of the first dimension of arrs is the same as the
     length of the first dimension of labels.
     """
-    assert(arrs.shape[0] == len(total_labels))
+    assert(len(arrs) == len(total_labels))
 
     masked_arrs = []
-    for i in range(0, len(total_labels)):
-        arr = arrs[i, ...]
+    for arr, labels in zip(arrs, total_labels):
         arr_shape = arr.shape
         ma_arr = np.ma.masked_array(arr,
                                     mask=np.zeros(arr_shape))
-
-        for label in total_labels[i]:
+        for label in labels:
             # label: x1x2y1y2
             # Set this label to True (which removes it from masked_array).
             ma_arr.mask[label[2]:label[3], label[0]:label[1]] = True
-
         masked_arrs.append(ma_arr)
+    return masked_arrs
 
     # Convert to stacked array
-    return np.ma.stack(tuple(masked_arrs))
+    #return np.ma.stack(tuple(masked_arrs))
 
 
 def save_synthesized(arrs: np.array, path: str, ext: str):
@@ -136,11 +148,14 @@ def save_synthesized(arrs: np.array, path: str, ext: str):
 
 
 def main():
-    arrs = load_images(INDIR, EXT)
-    total_labels = load_labels(os.path.join(INDIR, LABELS_FNAME))
+    #arrs = load_images(INDIR, EXT)
+    total_labels, arrs = load_labels_images(os.path.join(INDIR, LABELS_FNAME),
+                                            IMAGE_PATH)
     ma_arrs = mask_images(arrs, total_labels)
 
-    syns = scattering.synthesis('s_cov', ma_arrs, seed=0,
+    # Stack the arrays
+    ma_arrs = np.ma.stack(tuple(ma_arrs))
+    syns = scattering.synthesis('s_cov_iso', ma_arrs, seed=0,
                                 ensemble=True, N_ensemble=N,
                                 print_each_step=True)
 
