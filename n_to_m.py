9d2eb1cdcdd56ddb5af4c8840756da2a298eb92e
Nick Sullivan <nsulmol@gmail.com>
2024-11-17 16:23:48 -0500

    [feat] update n_to_m with extra options
    We now can do:
    - standard synthesis, where we feed a JSON with labels and pointing to
    associated images.
    - synthesis_images_only, where we feed a directory of images and
    synthesize them without removing any masks.
    - objects, where we feed a JSON with labels and pointing to associated
    images, and extract and save the objects.

--- n_to_m.py
+++ n_to_m.py
@@ -2,21 +2,16 @@ import os
 import json
 import numpy as np
 from PIL import Image
+import fire
 
 # Hack to import scattering
 import sys
 sys.path.append('./scattering_transform')
 import scattering
 
-import matplotlib.pyplot as plt
 
-# Parameters
-N = 10  # How many synthesis images to create
-INDIR = './data/'  # './data/'
-OUTDIR = './syn/'  # './syn/'
-EXT = '.png'
-LABELS_FNAME = 'labels.json'
-IMAGE_PATH = '/home/nsulmol/.local/share/label-studio/media/upload/1'
+# NOTE: Kept for convenience
+# IMAGE_PATH = '/home/nsulmol/.local/share/label-studio/media/upload/1'
 
 
 def normalize_image(arr: np.array) -> np.array:
@@ -47,11 +42,8 @@ def get_images_filepaths(path: str, ext: str = None) -> list[str]:
 
 def load_images_from_dir(path: str, ext: str = None) -> list[np.array]:
     """ Loads all images in a directory, normalizes, and returns."""
-    file_list = get_images_filepaths(path, str)
+    file_list = get_images_filepaths(path, ext)
     return load_images_from_file_list(file_list)
-
-    # Convert to stacked array
-    #return np.stack(tuple(arrs))
 
 
 def load_images_from_file_list(filepaths: list[str]) -> list[np.array]:
@@ -95,7 +87,6 @@ def load_labels_images(fname: str, img_path: str
         img_labels = []
 
         annotations = img_dict[ANNOTATIONS_STR][0][RESULT_STR]
-        # print(f'Annotations: {annotations}')
         for annotation in annotations:
             w = annotation[ORIG_WIDTH_STR]
             h = annotation[ORIG_HEIGHT_STR]
@@ -121,7 +112,7 @@ def mask_images(arrs: list[np.array], total_labels: list[list[int, int, int, int
     Assumes the shape of the first dimension of arrs is the same as the
     length of the first dimension of labels.
     """
-    assert(len(arrs) == len(total_labels))
+    assert len(arrs) == len(total_labels)
 
     masked_arrs = []
     for arr, labels in zip(arrs, total_labels):
@@ -135,11 +126,30 @@ def mask_images(arrs: list[np.array], total_labels: list[list[int, int, int, int
         masked_arrs.append(ma_arr)
     return masked_arrs
 
-    # Convert to stacked array
-    #return np.ma.stack(tuple(masked_arrs))
+
+def extract_objects(arrs: list[np.array], total_labels: list[list[int, int, int, int]]
+                ) -> list[np.array]:
+    """Exctract labeled objects from all provided arrays and save as list."""
+    assert len(arrs) == len(total_labels)
+
+    objects = []
+    for arr, labels in zip(arrs, total_labels):
+        arr_shape = arr.shape
+        for label in labels:
+            # label: x1x2y1y2
+            objects.append(arr[label[2]:label[3], label[0]:label[1]])
+    return objects
 
 
-def save_synthesized(arrs: np.array, path: str, ext: str):
+def save_arrs(arrs: list[np.array], path: str, ext: str):
+    """Save a list of numpy arrays to directory path with extension ext."""
+    for i in range(0, len(arrs)):
+        tmp = convert_to_uint16(arrs[i])
+        tmp = Image.fromarray(tmp)
+        tmp.save(os.path.join(path, str(i) + ext))
+
+
+def save_arr_stack_as_arrs(arrs: np.array, path: str, ext: str):
     """Save a numpy array stack in directory path with extension ext."""
     for i in range(0, arrs.shape[0]):
         tmp = convert_to_uint16(arrs[i, ...])
@@ -147,20 +157,112 @@ def save_synthesized(arrs: np.array, path: str, ext: str):
         tmp.save(os.path.join(path, str(i) + ext))
 
 
-def main():
-    #arrs = load_images(INDIR, EXT)
-    total_labels, arrs = load_labels_images(os.path.join(INDIR, LABELS_FNAME),
-                                            IMAGE_PATH)
+def synthesize_images_only(img_dir: str, synthesis_count: int,
+                           synth_style: str = 's_cov',
+                           img_ext: str = '.png') -> np.array:
+    """Analyze images and synthesize samples.
+
+    Args:
+        img_dir: path to the directory that contains the images to be analyzed.
+        synthesis_count: how many images to synthesize.
+        img_ext: image file extension, for filtering from img_dir.
+        synth_style: str indicating the style of analysis used for synthesis.
+
+    Returns:
+        synthesized images.
+    """
+    arrs = load_images_from_dir(img_dir, img_ext)
+    arrs = np.ma.stack(tuple(arrs))
+    syns = scattering.synthesis(synth_style, arrs, seed=0,
+                                ensemble=True, N_ensemble=N,
+                                print_each_step=True)
+    return syns
+
+
+def synthesize_images(json_path: str, img_dir: str, synthesis_count: int,
+                      synth_style: str = 's_cov') -> np.array:
+    """Analyze images with labels removed and synthesize samples.
+
+    Args:
+        json_path: path to the label-studio JSON file which contains ROIs for
+            each image that has been labeled.
+        img_dir: path to the directory that contains the images referenced
+            in the JSON file.
+        synthesis_count: how many images to synthesize.
+        synth_style: str indicating the style of analysis used for synthesis.
+
+    Returns:
+        synthesized images.
+    """
+    total_labels, arrs = load_labels_images(json_path, img_dir)
     ma_arrs = mask_images(arrs, total_labels)
 
     # Stack the arrays
     ma_arrs = np.ma.stack(tuple(ma_arrs))
-    syns = scattering.synthesis('s_cov_iso', ma_arrs, seed=0,
+    syns = scattering.synthesis(synth_style, ma_arrs, seed=0,
                                 ensemble=True, N_ensemble=N,
                                 print_each_step=True)
+    return syns
 
-    save_synthesized(syns, OUTDIR, EXT)
+
+def synthesize_save_images(json_path: str, img_dir: str, synthesis_count: int,
+                           synth_dir: str, synth_style: str = 's_cov',
+                           img_ext: str = '.png'):
+    """Analyze images with labels removed, synthesize samples and save.
+
+    Args:
+        json_path: path to the label-studio JSON file which contains ROIs for
+            each image that has been labeled.
+        img_dir: path to the directory that contains the images referenced
+            in the JSON file.
+        synthesis_count: how many images to synthesize.
+        synth_dir: path to the directory where the synthesized images will be
+            saved.
+        synth_style: str indicating the style of analysis used for synthesis.
+        img_ext: image file extension, for saving.
+    """
+    syns = synthesize_images(json_path, img_dir, synthesis_count, synth_style)
+    save_arr_stack_as_arrs(syns, synth_dir, img_ext)
 
 
-if __name__ == "__main__":
-    main()
+def synthesize_save_images_only(img_dir: str, synthesis_count: int,
+                                synth_dir: str, synth_style: str = 's_cov',
+                                img_ext: str = '.png'):
+    """Analyze images, synthesize samples and save.
+
+    Args:
+        img_dir: path to the directory that contains the images to be analyzed.
+        synthesis_count: how many images to synthesize.
+        synth_dir: path to the directory where the synthesized images will be
+            saved.
+        synth_style: str indicating the style of analysis used for synthesis.
+        img_ext: image file extension, for filtering from img_dir and saving.
+    """
+    syns = synthesize_images_only(img_dir, synthesis_count, synth_style, img_ext)
+    save_arr_stack_as_arrs(syns, synth_dir, img_ext)
+
+
+def save_objects_from_images(json_path: str, img_dir: str, out_dir: str,
+                             img_ext: str = '.png'):
+    """Extract labeled objects from images and save to dir.
+
+    Args:
+        json_path: path to the label-studio JSON file which contains ROIs for
+            each image that has been labeled.
+        img_dir: path to the directory that contains the images referenced
+            in the JSON file.
+        out_dir: path to the directory where the object images will be
+            saved.
+        img_ext: image file extension, for saving.
+    """
+    total_labels, arrs = load_labels_images(json_path, img_dir)
+    objects = extract_objects(arrs, total_labels)
+    save_arrs(objects, out_dir, img_ext)
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        'synth': synthesize_save_images,
+        'synth_images_only': synthesize_save_images_only,
+        'objects': save_objects_from_images
+    })
